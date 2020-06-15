fd25cd9c1fc3b9ea541c2cc25205af60e7c706eb
akash1049 <akashngangani14@gmail.com>
2020-06-15 16:13:30 +0530

    states issue in user form solved

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -7,7 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormComponent  {
 
+  public name: string ;
+  public email: string ;
+  public password: string ;
+  public states: string ;
+  public symptoms: string ;
   
-  states : [{id : 1 , value: 'Gujarat'}];
+  states_list = [
+    { id:'Andaman and Nicobar' , value: 'Andaman and Nicobar'},
+    { id:'Andhra Pradesh',    value : 'Andhra Pradesh'    },
+    { id:'Arunachal Pradesh', value : 'Arunachal Pradesh' },
+    { id:'Assam', value : 'Assam' },
+    { id:'Bihar', value : 'Bihar' },
+    { id:'Chandigarh' , value: 'Chandigarh'},
+    { id:'Chhattisgarh', value : 'Chhattisgarh' },
+    { id:'Daman and Diu' , value: 'Daman and Diu'},
+    { id:'Dadar and Nagar Haveli' , value: 'Dadar and Nagar Haveli'},
+    { id:'Delhi' , value: 'Delhi'},
+    { id:'Goa', value : 'Goa' },
+    { id:'Gujarat', value : 'Gujarat' },
+    { id:'Haryana', value : 'Haryana' },
+    { id:'Himachal Pradesh', value : 'Himachal Pradesh' },
+    { id:'Jammu and Kashmir' , value: 'Jammu and Kashmir'},
+    { id:'Jharkhand', value : 'Jharkhand' },
+    { id:'Karnataka', value : 'Karnataka' },
+    { id:'Kerala', value : 'Kerala' },
+    { id:'Ladakh' , value: 'Ladakh'},
+    { id:'Lakshadweep' , value: 'Lakshadweep'},
+    { id:'Madhya Pradesh', value : 'Madhya Pradesh' },
+    { id:'Maharashtra', value : 'Maharashtra' },
+    { id:'Manipur', value : 'Manipur' },
+    { id:'Meghalaya', value : 'Meghalaya' },
+    { id:'Mizoram', value : 'Mizoram' },
+    { id:'Nagaland', value : 'Nagaland' },
+    { id:'Odisha', value : 'Odisha' },
+    { id:'Puducherry' , value: 'Puducherry'},
+    { id:'Punjab', value : 'Punjab' },
+    { id:'Rajasthan', value : 'Rajasthan' },
+    { id:'Sikkim', value : 'Sikkim' },
+    { id:'TamilNadu', value : 'TamilNadu' },
+    { id:'Telangana', value : 'Telangana' },
+    { id:'Tripura', value : 'Tripura' },
+    { id:'Uttar Pradesh', value : 'Uttar Pradesh' },
+    { id:'Uttarakhand', value : 'Uttarakhand' },
+    { id:'West Bengal', value : 'West Bengal' }
+  ];
 
 }
