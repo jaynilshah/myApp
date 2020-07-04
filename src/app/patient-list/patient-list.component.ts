246bbc728fdfe20d4af1ba29d6e25c5dc2badcfe
akash1049 <akashngangani14@gmail.com>
2020-07-04 14:55:02 +0530

    Modified tile structure

--- src/app/patient-list/patient-list.component.ts
+++ src/app/patient-list/patient-list.component.ts
@@ -11,7 +11,8 @@ export class PatientListComponent implements OnInit {
   constructor() { }
   //make list of patient like mock state here
   patients_list: patientDetail[] = [
-    {name:"Jay" , email:"jay@gmail.com" , state:"Gujarat" , symptoms:"Fever" , active:true , deceased:false , recovered:false}
+    {name:"Jay" , email:"jay@gmail.com" , state:"Gujarat" , symptoms:"Fever" , active:true , deceased:false , recovered:false},
+    {name:"Suy" , email:"suy@gmail.com" , state:"Gujarat" , symptoms:"Fever, Cough" , active:true , deceased:false , recovered:false}
   ]
   ngOnInit(): void {
   }
